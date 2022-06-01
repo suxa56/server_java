89562f580f90b39ed132e69f9365133eda95d9a5
suxa56 <62641532+suxa56@users.noreply.github.com>
2022-06-01 15:56:17 +0500

    Service interface

---
+++ src/main/java/com/example/server/service/ServerService.java
@@ -0,0 +1,15 @@
+package com.example.server.service;
+
+import com.example.server.model.Server;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public interface ServerService {
+    Server create(Server server);
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+}
