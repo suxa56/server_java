d8c0b3d87ff51478073b494f485f24e40e1cfb13
suxa56 <62641532+suxa56@users.noreply.github.com>
2022-06-06 09:23:52 +0500

    Save and ping mappings

--- src/main/java/com/example/server/controller/ServerController.java
+++ src/main/java/com/example/server/controller/ServerController.java
@@ -1,13 +1,15 @@
 package com.example.server.controller;
 
+import com.example.server.enumiration.Status;
 import com.example.server.model.Response;
+import com.example.server.model.Server;
 import com.example.server.service.ServerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -29,7 +31,42 @@ public class ServerController {
                         HttpStatus.OK.value(),
                         HttpStatus.OK,
                         "Server retrieved",
-                        Map.of("servers", serverService.list(30)))
+                        Map.of("servers", serverService.list(30))
+                )
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(
+            @PathVariable("ipAddress")
+            String ipAddress
+    ) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                new Response(
+                        LocalDateTime.now(),
+                        HttpStatus.OK.value(),
+                        HttpStatus.OK,
+                        server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed",
+                        Map.of("server", server)
+                )
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(
+            @RequestBody
+            @Valid
+            Server server
+    ) {
+        return ResponseEntity.ok(
+                new Response(
+                        LocalDateTime.now(),
+                        HttpStatus.CREATED.value(),
+                        HttpStatus.CREATED,
+                        "Server created",
+                        Map.of("server", serverService.create(server))
+                )
         );
     }
 }
